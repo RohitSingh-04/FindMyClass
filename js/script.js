f45954cf475fa74797cb4bddc4e1e88d85bf9f5c
ShubhamSinghRawat007 <127177695+ShubhamSinghRawat007@users.noreply.github.com>
2024-03-08 22:37:28 +0530

    added dark mode.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
-const searchBar = document.getElementById("class")
-
+const searchBar = document.getElementById("input-txt")
+const darkBtn = document.getElementById("dark-btn")
+let moon = document.getElementById("moon-icon")
+const logo = document.getElementById("class-finder-logo")
+const logoTop =document.getElementById("top-logo")
+let flag = 1
 function onexit(){
     if(searchBar.value==''){
         searchBar.value="Enter class to search"
@@ -8,7 +12,9 @@ function onexit(){
 }
 
 searchBar.addEventListener("focus",()=>{
+    if(searchBar.value=="Enter class to search"){
     searchBar.value=""
+    }
 })
 
 searchBar.addEventListener("blur",()=>{
@@ -18,5 +24,22 @@ searchBar.addEventListener("blur",()=>{
 searchBar.addEventListener("keypress", (event)=>{
     if (event.key=="Enter"){
         onexit();
+    }
+})
+darkBtn.addEventListener("click",()=>{
+    if(flag==1){
+    document.body.style.backgroundColor="#212121"
+    logo.src="/img/LOGO_DARK.png"
+    logoTop.src="/img/LOGO6.png"
+    moon.style.color="#5e17eb"
+    
+    flag = 0
+    }
+    else{
+        document.body.style.backgroundColor="lightgrey"
+        logo.src="/img/mainLogo.png"
+        logoTop.src="/img/LOGO-2.png"
+        moon.style.color="#5e17eb"
+        flag = 1
     }
 })
