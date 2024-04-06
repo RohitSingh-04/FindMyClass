e9ff2d0fb1235c927b58128c01e4b605a0987fe0
Rohit Singh <135635405+RohitSingh-04@users.noreply.github.com>
2024-04-06 23:45:42 +0530

    Missing Page Connected with Database and Added Favicon

--- js/addReq.js
+++ js/addReq.js
@@ -1,3 +1,20 @@
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
+
+const cred = {
+    url: "https://agomwuylpfnaszvybavj.supabase.co",
+    key: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFnb213dXlscGZuYXN6dnliYXZqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDk4MTE4NTgsImV4cCI6MjAyNTM4Nzg1OH0.m1V8gwMXysoKafHHfWwAlr4n_7VQIgiY0bOxk6UsTX4"
+}
+
+const supabase = createClient(cred.url, cred.key)
+
+async function write(data){
+    const { error } = await supabase
+    .from('requests')
+    .insert(data);
+
+    return error;
+}
+
 const btn = document.getElementById("Submit")
 
 function fetchData(){
@@ -5,16 +22,17 @@ function fetchData(){
     const floor=document.getElementById("floor")
     const block=document.getElementById("block")
 
-    return {name:name.value, floor:floor.value, block:block.value}
-
+    return {Class_Name:name.value, Floor:floor.value, Block:block.value}
 }
 
 btn.addEventListener("click", ()=>{
     let a = fetchData()
     if (a.name == "" || a.floor == "" || a.block==""){
+        console.log('notdone')
         return ;
     }
-    // if nothing is missing than
-    //logic for adding data to db
+    console.log('done1')
+    write(a);
+
 })
 
